68fa78dcceab7a4a4441991bfc71edde105695c9
Octavian <octavian.c96@gmail.com>
2020-06-02 16:33:33 +0300

    Added skeleton for app

---
+++ src/test/java/com/appsdev/PaymentSystem/PaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdev.PaymentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
